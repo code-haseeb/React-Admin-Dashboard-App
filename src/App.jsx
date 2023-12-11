8f74d4baecf29b216a3c9c56c07288606e42cf6d
Abdull-Haseeb <abdullhaseebonline@gmail.com>
2023-12-12 01:29:10 +0500

    adding global sidebar scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+// import { useState } from "react";
+import { useMode, ColorModeContext } from "./theme";
+import { ThemeProvider, CssBaseline } from "@emotion/react";
+
 function App() {
+  const [theme, colorMode] = useMode(true);
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <>
-      <div>aoa </div>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app"></div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
