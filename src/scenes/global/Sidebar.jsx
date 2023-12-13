0c59c971fe0bde2b68e7d220880241ce181300d0
Abdull-Haseeb <abdullhaseebonline@gmail.com>
2023-12-13 18:44:29 +0500

    adding modifying package-lock.json file

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,10 +107,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Abdul Haseeb
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  
                 </Typography>
               </Box>
             </Box>
